4984f6b74062b358c35953a2856bb5fd2f67cca1
Samuel Aguilar <isc.samuel.aguilar@gmail.com>
2024-01-16 19:42:43 -0600

    first changes

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -3,27 +3,26 @@ import "./formulario.css"
 
 import { useState } from "react"
 export function Login({setuser}){
-    const [usuario, setusuario]= useState("")
-    const [contraseña , setcontraseña]= useState("")
+    const [User, setUser]= useState("")
+    const [contraseña , setPassword]= useState("")
     const [error, seterror]= useState(false)
-    const password={
-        contraseña: /^.{10}$/
-    }
+
     const handleSubmit= (e) =>{
         e.preventDefault()
-        if(usuario =="" || contraseña =="" ){
+        if(User === "" || contraseña === "" ){
             seterror(true)
             return
         }
         seterror(false)
-        setuser([usuario])
+        setuser([User])
     }
+
     return (
         <section className="formulario">
             <h1> Login</h1>
             <form action="" className="formulario" onSubmit={handleSubmit} >
-                <input type="text" placeholder="Usuario" value={usuario} onChange={e=> setusuario(e.target.value)}/>
-                <input type="password" placeholder="Contaseña" pattern="[0-9]*" value={contraseña.password} onChange={e=> setcontraseña(e.target.value)}/>
+                <input type="text" placeholder="Usuario" value={User} onChange={e=> setUser(e.target.value)}/>
+                <input type="password" placeholder="Contaseña" pattern="[0-9]*" value={contraseña.password} onChange={e=> setPassword(e.target.value)}/>
                 <Link to={"/home"}><button href="./home">iniciar sesion</button></Link>
                 
             </form>
